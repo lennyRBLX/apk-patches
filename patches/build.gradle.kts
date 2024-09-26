8e6cae1e2ef9d8cb98f8a4d85ac2d9551946bc11
frieren <38077688+lennyRBLX@users.noreply.github.com>
2024-09-26 16:32:22 -0400

    Update build.gradle.kts

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -11,3 +11,43 @@ patches {
         license = "GNU General Public License v3.0"
     }
 }
+
+dependencies {
+    // Used by JsonGenerator.
+    implementation(libs.gson)
+    // Required due to smali, or build fails. Can be removed once smali is bumped.
+    implementation(libs.guava)
+}
+
+tasks {
+    jar {
+        exclude("app/revanced/generator")
+    }
+
+    register<JavaExec>("generatePatchesFiles") {
+        description = "Generate patches files"
+
+        dependsOn(build)
+
+        classpath = sourceSets["main"].runtimeClasspath
+        mainClass.set("app.revanced.generator.MainKt")
+    }
+
+    // Used by gradle-semantic-release-plugin.
+    publish {
+        dependsOn("generatePatchesFiles")
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/lennyRBLX/apk-patches")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
