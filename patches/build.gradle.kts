a5e3eb5101de4795552b97bdb36d014c9c0f1a9e
frieren <38077688+lennyRBLX@users.noreply.github.com>
2024-09-26 14:58:34 -0400

    Update build.gradle.kts

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -1,13 +1,13 @@
-group = "app.revanced"
+group = "patches.lennyrblx"
 
 patches {
     about {
-        name = "ReVanced Patches template"
-        description = "Patches template for ReVanced"
-        source = "git@github.com:revanced/revanced-patches-template.git"
-        author = "ReVanced"
-        contact = "contact@revanced.app"
-        website = "https://revanced.app"
+        name = "APK Patches"
+        description = "Personal patches for various Android APKs"
+        source = "git@github.com:lennyRBLX/apk-patches.git"
+        author = "lennyRBLX"
+        contact = ""
+        website = ""
         license = "GNU General Public License v3.0"
     }
 }
